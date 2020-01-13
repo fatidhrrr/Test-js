2dd3cbeb310e720bc25fb6d1526f976173ceb21c
Dahri Fati <mohaadhr@MacBook-Air-de-Dahri.local>
2020-01-13 14:12:33 +0100

    fin

---
+++ js/index.js
@@ -0,0 +1,126 @@
+// 2
+
+// 3 monArray
+
+let monArray = []
+
+// 4 monObjet
+
+let monObjet = {
+    prenom: "Fati"
+}
+
+// 5 monString
+
+let monString = "Hello Fati"
+
+// 6 integer
+
+let monInt = 3;
+
+// 7 objet et sa méthode 
+
+let personne = {
+    nom: "Dahri",
+    changer()
+}
+
+// 8 function
+
+function maFunction(a,b) {
+
+}
+
+// 9 condition
+
+    if (monInt > 8) {
+        console.log("est plus petit que 8");
+    } else {
+        console.log("est plus grand que 8");
+    }
+
+// 10 boucle
+
+let vari = 1
+
+for (let i = 0; i < 10; i++) {
+    console.log(vari * i);
+}
+
+// 11 objet date 
+
+let dateJour = {
+    date = "13/01/2020"
+}
+console.log(dateJour);
+// C'etait une date dynamique qui était attendue avec lobjet new Date() là tu as mis une date static cest a dire que demain elle sera fausse
+// 12 string to STRING
+
+function maFunct(c,d) {
+    let string = "Fati";
+    let string1 = string.toUpperCase();
+}
+console.log(string1);
+
+// 13 string to StrinG
+
+// 14 longueur
+
+function long() {
+    alert("")
+}
+
+// 15 chiffre aléatoire p1
+
+// 16 chiffre aléatoire p2
+
+// 17 type
+
+// 18 objet 1/1
+
+let personnage = {
+    nom : "",
+    age : "",
+    modeling(){
+        alert(`Bonjour ${personnage.nom}, tu as ${personnage.age} ans`);
+    }
+}
+personnage.nom = "Dahri"
+personnage.age = 20
+personnage.modeling()
+console.log(personnage);
+
+// 19 objet 2/2
+
+let personnage1 = {
+    addKey() {
+        personnage.nom
+    }
+}
+console.log(personnage);
+
+// 20 objet
+
+let carre = {
+    size: "",
+    borderaspect: "",
+    epaisseur: "",
+    couleur: "",
+    valeur() {
+
+    }
+}
+carre.size = "15px"
+carre.borderaspect = "solide"
+carre.epaisseur = 16
+carre.couleur = "rouge"
+
+if (carre.borderaspect != solide || dotted) {
+    alert("Entrez une valeur correcte");
+} else if (carre.couleur != red || blue || green) {
+    alert("Entrez une valeur correcte");
+} else if (carre.epaisseur)
+
+// 21 HTML
+
+// 22 DOM injection 
